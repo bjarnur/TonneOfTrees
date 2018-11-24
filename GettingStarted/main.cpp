2efea61d8b553b93bbe35a00138a5ca5f505a812
Bjarni <bjarniragnarg@gmail.com>
2018-11-24 14:42:41 +0100

    Code cleanup

--- GettingStarted/main.cpp
+++ GettingStarted/main.cpp
@@ -57,15 +57,23 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 */
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 
-void render_model();
+/**
+Convertes texture to an image (NOTE: hardocded to GL_COLOR_ATTACHMENT0 at the moment) */
+void texture_to_image(bool flip_image);
 
+/**
+Set's up frame buffer for rendering to texture */
 GLuint get_texture_framebuffer(GLuint& tex_color_buffer);
+
+/**
+*/
+void set_buffers_for_quad_render(GLuint & quadVAO, GLuint & quadVBO);
 
 /******************************\
 	Function Definitions
 \******************************/
 
-int main()
+void set_buffers_for_quad_render(GLuint & quadVAO, GLuint & quadVBO)
 {
 	float quadVertices[] = { // vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates.
 							 // positions   // texCoords
@@ -76,15 +84,8 @@ int main()
 		-1.0f,  1.0f,  0.0f, 1.0f,
 		1.0f, -1.0f,  1.0f, 0.0f,
 		1.0f,  1.0f,  1.0f, 1.0f
-	};	
+	};
 
-	GLFWwindow * window = initialize_window();
-	if (!window)
-	{
-		return -1;
-	}
-
-	GLuint quadVAO, quadVBO;
 	glGenVertexArrays(1, &quadVAO);
 	glGenBuffers(1, &quadVBO);
 	glBindVertexArray(quadVAO);
@@ -95,19 +96,33 @@ int main()
 	glEnableVertexAttribArray(1);
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
 
+	glBindVertexArray(0);
+}
 
-	//Configure shader program used by OpenGL
+int main()
+{
+	GLFWwindow * window = initialize_window();
+	if (!window)
+	{
+		return -1;
+	}
+
+	//Setting up carmera
 	Camera camera(glm::vec3(0.0, 0.0, 3.0), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
+	
+	//Configure shader program used by OpenGL
 	Shader shader_program = Shader("shaders\\vertex_shader.txt", "shaders\\fragment_shader.txt");
 	Shader screen_shader = Shader("shaders\\tex_vertex_shader.txt", "shaders\\tex_fragment_shader.txt");
-
 	screen_shader.use();
 	screen_shader.setInt("screenTexture", 0);
 
 	//Configure mouse attributes
-	//glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-
-	Model nano_model("D:\\Bjarni\\Projects\\3Dmodels\\nanosuit\\nanosuit.obj");
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	
+	//Preparing models to render
+	GLuint quadVAO, quadVBO;
+	set_buffers_for_quad_render(quadVAO, quadVBO);
+	Model nano_model("D:\\Bjarni\\Projects\\3Dmodels\\nanosuit\\nanosuit.obj");	
 
 	//Preparing texture
 	GLuint tex_color_buffer;
@@ -124,10 +139,9 @@ int main()
 		process_input(window, camera);
 		camera.update(mouse.pitch, mouse.yaw);
 
-		//Clearing screen
+		//Setup for render to texture
 		glBindFramebuffer(GL_FRAMEBUFFER, texture_framebuffer);
 		glEnable(GL_DEPTH_TEST);
-
 		glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		shader_program.use();
@@ -152,15 +166,16 @@ int main()
 		shader_program.setMat4("model", model);
 		
 		nano_model.draw(shader_program);
+		
+		//Setup for render to window
 		glBindVertexArray(0);
-
 		glBindFramebuffer(GL_FRAMEBUFFER, 0);
 		glDisable(GL_DEPTH_TEST);
-
 		glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-
 		screen_shader.use();
+
+		//Render a single quad
 		glBindVertexArray(quadVAO);		
 		glBindTexture(GL_TEXTURE_2D, tex_color_buffer);
 		glDrawArrays(GL_TRIANGLES, 0, 6);
@@ -169,73 +184,65 @@ int main()
 		glfwPollEvents();
 	}
 
-	FILE    *output_image;
-	int     output_width, output_height;
-
-	output_width = SCR_WIDTH;
-	output_height = SCR_HEIGHT;
-
-	/// READ THE PIXELS VALUES from FBO AND SAVE TO A .PPM FILE
-	int             i, j, k;
-	unsigned char   *pixels = (unsigned char*)malloc(output_width*output_height * 3);
-
-	/// READ THE CONTENT FROM THE FBO
-	glReadBuffer(GL_COLOR_ATTACHMENT0);
-	glReadPixels(0, 0, output_width, output_height, GL_RGB, GL_UNSIGNED_BYTE, pixels);
-
-	output_image = fopen("D:\\Bjarni\\Projects\\3Dmodels\\output.ppm", "wt");
-	fprintf(output_image, "P3\n");
-	fprintf(output_image, "# Created by Ricao\n");
-	fprintf(output_image, "%d %d\n", output_width, output_height);
-	fprintf(output_image, "255\n");
-
-	k = 0;
-	for (i = 0; i<output_width; i++)
-	{
-		for (j = 0; j<output_height; j++)
-		{
-			fprintf(output_image, "%u %u %u ", (unsigned int)pixels[k], (unsigned int)pixels[k + 1],
-				(unsigned int)pixels[k + 2]);
-			k = k + 3;
-		}
-		fprintf(output_image, "\n");
-	}
-	free(pixels);
+	//Save current state of the frame buffer texture
+	texture_to_image(true);
 
 	glfwTerminate();
 	return 0;
 }
 
+void texture_to_image(bool flip_image)
+{
+	FILE *output_image;
+	int output_width = SCR_WIDTH;
+	int output_height = SCR_HEIGHT;
+	std::vector<unsigned char>	pxls(output_width * output_height * 3);
+
+	glReadBuffer(GL_COLOR_ATTACHMENT0);	
+	glReadPixels(0, 0, output_width, output_height, GL_RGB, GL_UNSIGNED_BYTE, &pxls[0]);
+
+	output_image = fopen("D:\\Bjarni\\Projects\\3Dmodels\\output.ppm", "wt");
+	fprintf(output_image, "P3\n");
+	fprintf(output_image, "# Created by brgud\n");
+	fprintf(output_image, "%d %d\n", output_width, output_height);
+	fprintf(output_image, "255\n");
+
+	std::vector<unsigned char> flipped(output_width * output_height * 3);
+	for (int i = 0; i < output_height; i++)
+	{
+		memcpy(&flipped[i * output_width * 3],						// address of destination
+			&pxls[(output_height - i - 1) * output_width * 3],		// address of source
+			output_width * 3 * sizeof(unsigned char));				// number of bytes to copy
+	}
+
+	int k = 0;
+	for (int i = 0; i<output_width; i++)
+	{
+		for (int j = 0; j<output_height; j++)
+		{
+			if (flip_image)
+			{
+				fprintf(output_image, "%u %u %u ", 
+						(unsigned int)flipped[k], 
+						(unsigned int)flipped[k + 1],
+						(unsigned int)flipped[k + 2]);
+			}			
+			else
+			{
+				fprintf(output_image, "%u %u %u ",
+					(unsigned int)pxls[k],
+					(unsigned int)pxls[k + 1],
+					(unsigned int)pxls[k + 2]);
+			}
+			k = k + 3;
+		}
+		fprintf(output_image, "\n");
+	}
+	fclose(output_image);
+}
 
 GLuint get_texture_framebuffer(GLuint& tex_color_buffer)
 {
-	/*
-	//Initializing frame buffer
-	GLuint framebuffer_name = 0;
-	glGenFramebuffers(1, &framebuffer_name);
-	glBindFramebuffer(GL_FRAMEBUFFER, framebuffer_name);
-
-	glBindTexture(GL_TEXTURE_2D, tex_color_buffer);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR); //GL_NEAREST
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); //GL_NEAREST		
-
-	//glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, rendered_texture, 0);
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, tex_color_buffer, 0);
-	//glBindTexture(GL_TEXTURE_2D, 0);
-
-
-	//Depth attachment to the frame buffer
-	GLuint depth_redner_buffer;
-	glGenRenderbuffers(1, &depth_redner_buffer);
-	glBindRenderbuffer(GL_RENDERBUFFER, depth_redner_buffer);
-	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depth_redner_buffer);	
-	//glBindRenderbuffer(GL_RENDERBUFFER, 0);
-
-	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depth_redner_buffer);
-	*/
-
-
 	unsigned int framebuffer;
 	glGenFramebuffers(1, &framebuffer);
 	glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
@@ -247,6 +254,7 @@ GLuint get_texture_framebuffer(GLuint& tex_color_buffer)
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, tex_color_buffer, 0);
+
 	// create a renderbuffer object for depth and stencil attachment (we won't be sampling these)
 	unsigned int rbo;
 	glGenRenderbuffers(1, &rbo);
